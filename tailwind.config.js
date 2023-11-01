b7e6600798c71684a69d68fb99df15c7666baea5
tamzidpeace <tamjedpeace@gmail.com>
2023-11-01 11:37:27 +0600

    support added for m2 chip

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,33 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cLimadiTopBar: '#F89818',
+        cBrand: '#F89818',
+        cBgSideBar: '#FFFAF2',
+        cBgSelectedSideBar: '#FFF1DF',
+        cSidebarDarkBg: "#1F2937",
+        cBrandColor2: "#FFFFFF",
+        cTextGray: "#787878",
+        cBgLayout: "#EEEEEE",
+        cMainBlue: "#2257AA",
+        cMainWhite: '#FFFFFF',
+        cCommonListBG: "#D3E5FF",
+        cCommonListIconColor: "#A3B7D5",
+        cMainBlack: "#0E1826",
+        cSecondaryTextColor: "#333333",
+        cRed: "#FF6368",
+        cDisable: '#89919E',
+        cTextButtonHover: '#D1D5DB',
+        cLightSkyBlue: "#96C0FF",
+        cCustomerBlack: " #0E1826",
+        cGrey: "#89919E",
+        cFloralWhite: '#FFFAF2',
+        cLightGrayishBlue: '#D1D5DB',
+        cBackground: "#e2e2e2",
+      }
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
