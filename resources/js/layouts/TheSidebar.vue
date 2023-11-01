d8ea18a35ade9bb5fe0612969808592abcde3b93
tamzidpeace <tamjedpeace@gmail.com>
2023-11-01 18:55:31 +0600

    design improvement

--- resources/js/layouts/TheSidebar.vue
+++ resources/js/layouts/TheSidebar.vue
@@ -6,7 +6,6 @@
     >
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
             <ul class="space-y-2 font-medium">
-
                 <SidebarSingleLink
                     label="Dashboard"
                     href="/"
@@ -24,66 +23,50 @@
                 </SidebarSingleLink>
 
                 <li>
-                    <button
-                        type="button"
-                        class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                        aria-controls="dropdown-example"
-                        data-collapse-toggle="dropdown-example"
-                    >
-                        <svg
-                            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 18 21"
+                    <div class="flex flex-col justify-start space-y-2">
+                        <div
+                            class="flex justify-between items-center cursor-pointer hover:bg-gray-100 p-2 rounded-md"
+                            @click="rotate = !rotate"
                         >
-                            <path
-                                d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"
+                            <div
+                                class="flex justify-start space-x-3 items-center"
+                            >
+                                <InformationCircleIcon class="h-5 w-5" />
+                                <span class="text-sm tracking-wide truncate"
+                                    >Multi Level</span
+                                >
+                            </div>
+
+                            <ChevronDownIcon
+                                class="h-5 w-5 transition-all delay-150 duration-200 ease-in-out"
+                                :class="rotate ? 'rotate-180' : ''"
                             />
-                        </svg>
-                        <span
-                            class="flex-1 ml-3 text-left whitespace-nowrap text-sm"
-                            >E-commerce</span
+                        </div>
+
+                        <div
+                            v-if="rotate"
+                            class="flex flex-col justify-start space-y-2"
                         >
-                        <svg
-                            class="w-3 h-3"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 10 6"
-                        >
-                            <path
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="m1 1 4 4 4-4"
-                            />
-                        </svg>
-                    </button>
-                    <ul id="dropdown-example" class="hidden py-2 space-y-2">
-                        <li>
-                            <a
-                                href="#"
-                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                                >Products</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                                >Billing</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                                >Invoice</a
-                            >
-                        </li>
-                    </ul>
+                            <div class="ml-8">
+                                <SidebarSingleLink
+                                    label="Dashboard"
+                                    href="/"
+                                    :active="$page.url === '/'"
+                                >
+                                    <Squares2X2Icon class="h-5 w-5" />
+                                </SidebarSingleLink>
+                            </div>
+                            <div class="ml-8">
+                                <SidebarSingleLink
+                                    label="About"
+                                    href="/about"
+                                    :active="$page.url === '/about'"
+                                >
+                                    <InformationCircleIcon class="h-5 w-5" />
+                                </SidebarSingleLink>
+                            </div>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -92,12 +75,17 @@
 
 <script setup lang="ts">
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
 import {
     Squares2X2Icon,
     HomeIcon,
     InformationCircleIcon,
+    ChevronDownIcon,
 } from "@heroicons/vue/24/solid";
-import SidebarSingleLink from "./partials/SidebarSingleLink.vue";
+import SidebarSingleLink from "./partials/SidebarSingleLevelMenu.vue";
+import PanelMenu from "primevue/panelmenu";
+
+const rotate = ref(false);
 </script>
 
 <style scoped></style>
--- resources/js/layouts/partials/SidebarSingleLevelMenu.vue
+++ resources/js/layouts/partials/SidebarSingleLevelMenu.vue
@@ -3,7 +3,7 @@
         <Link
             :href="href"
             class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-            :class="{ 'bg-gray-100': active }"
+            :class="{ 'bg-gray-100 text-blue-500': active }"
         >
             <slot> </slot>
             <span class="ml-3 text-sm tracking-wide truncate">{{ label }}</span>
